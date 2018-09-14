76db52d47194a91537ae802600c82958af9d80f5
Pixelastic <tim@pixelastic.com>
2018-09-14 20:27:21 +0200

    use firost

---
+++ scripts/3-refine-text.js
@@ -0,0 +1,9 @@
+import refiner from '../src/refiner';
+import firost from 'firost';
+console.info(firost);
+
+// get all txt files path in order from the directory
+// for each, refine them, and seav ethem
+
+
+console.info(refiner);
---
+++ src/refiner.js
@@ -0,0 +1,5 @@
+import _ from 'lodash';
+
+const module = {};
+
+export default _.bindAll(module, _.functions(module));
