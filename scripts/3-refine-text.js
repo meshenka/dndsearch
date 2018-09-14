dc6f56aae12cb73e62c33c2acebeb9658bbd8b72
Pixelastic <tim@pixelastic.com>
2018-09-14 20:52:11 +0200

    Update scaffold

--- scripts/3-refine-text.js
+++ scripts/3-refine-text.js
@@ -1,9 +1,11 @@
 import refiner from '../src/refiner';
 import firost from 'firost';
-console.info(firost);
 
-// get all txt files path in order from the directory
-// for each, refine them, and seav ethem
+(async () => {
+  const rawPaths = await firost.glob('./tmp/player-handbook/txt-raw/*.txt');
 
-
-console.info(refiner);
+  // Read raw files by group of 5 in parallel
+  // Transform content into list of nodes, with type and value
+  // Save it into json file
+  console.info(rawPaths);
+})();
